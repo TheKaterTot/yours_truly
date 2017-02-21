2785fd6ea043f2dc25732c502e5c8861ab3ba1a6
Katy Farmer <dreadpiratekaty@gmail.com>
2017-02-20 21:32:08 -0700

    Adds templates to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+c1 = Category.create(name: "Undying Love")
+c2 = Category.create(name: "We're Breaking Up")
+c3 = Category.create(name: "Insults")
+
+Template.create(title: "My Dearest", content: "I used to dream of a quiet place
+  in the trees, a place where the sun barely broke through. There was gentle water
+  and still branches, and cool air that quenced a thirst I did not know I had. But
+  you, my love, have rendered this dream obsolete. You are perfectly yourself,
+  as natural as the stream in your grace. I listen to your breath like
+  the air in the forest, and it is the music in my heart. You have become something
+  more than any dream could be. You are natural and everlasting. You are my quiet
+  place.", category: c1)
+
+Template.create(title: "Villain", content: "I have heard of your actions, you
+  scullion. You are the best of cutthroats, and I should have known you had it in
+  you. I remember well my first impression of you: a most notable coward. Had I not
+  been convinced of your integrity by our mutual acquantaince, I could have saved
+  myself the trouble of your friendship. I know better now, and I will not make
+  the mistake again of granting you such favor. May we be better strangers in the
+  future.", category: c3)
+
+Template.create(title: "Stardust", content: "I can hardly believe that I am made of
+  starstuff. I know so little about this world or any of the worlds beyond it. I
+  am ignorant of how I could be so complex when the things I say and feel are so
+  simple. Yet, it makes sense that you are celestial--you who knows so much,
+  who stays awake at night with your family in the stars. Your mind is as expansive
+  as the night sky, and you are as much a part of it as any other star. You are
+  truly stardust, and you are making me believe that I am too.", category: c1)
+
+Template.create(title: "Yeah...No", content: "We're breaking up. This must come
+  as a surprise to you, given how difficult it is for you to hear voices
+  that are not your own. Had you been listening, you would have heard me try to
+  end things last week, but you were distracted by your reflection in the cafe window.
+  I understand. However briefly, I too was distracted by your face. I had hoped
+  it could make up for all the things you said and did, but in fact, no amount of
+  please symmetry could make you tolerable. I hope that you can read better than
+  you can listen. We're over.", category: c2)
